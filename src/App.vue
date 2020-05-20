c3fb3be360393b80806b905300c418d6e64695e1
Oberst47 <oberst47@gmail.com>
2020-05-20 17:57:43 +0300

    chore: created a layout

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,13 @@
 <template>
     <div id="app" class="main">
-        <header>
-            <SectionHeader/>
-        </header>
+        <router-view/>
     </div>
 </template>
 
 <script>
-import SectionHeader from '@/components/sections/header'
 
 export default {
   components: {
-    SectionHeader
   }
 }
 </script>
---
+++ src/layouts/main/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        main-layout
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style scoped lang="scss">
+
+</style>
