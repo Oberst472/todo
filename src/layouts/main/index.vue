68379c7d7ff0f15274af5f986dba2cb5fc8e7aa1
Oberst47 <oberst47@gmail.com>
2020-05-20 18:07:13 +0300

    chore: connected the router

--- src/layouts/main/index.vue
+++ src/layouts/main/index.vue
@@ -1,11 +1,21 @@
 <template>
-    <div>
-        main-layout
+    <div class="layout-main">
+        <header class="layout-header">
+            <SectionHeader/>
+        </header>
+        <main class="layout-main">
+            <router-view/>
+        </main>
     </div>
 </template>
 
 <script>
-    export default {}
+    import SectionHeader from '@/components/sections/header';
+    export default {
+        components: {
+            SectionHeader
+        }
+    }
 </script>
 
 <style scoped lang="scss">
---
+++ src/router.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+export default new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '',
+            component: require('./layouts/main').default,
+            children: [
+                {
+                    path: '/',
+                    name: 'main',
+                    components: {
+                        default: require('./pages/main').default
+                    }
+                }
+            ]
+        }
+    ]
+})
