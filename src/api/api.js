b3358c5f10b7faa21d455bc96b9e994a23adbc53
Oberst47 <oberst47@gmail.com>
2020-05-22 20:44:22 +0300

    refactor: fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import customFetch from './fetch'
 
-const baseUrl = 'http://localhost:3000/api'
+const baseUrl = process.env.NODE_ENV === 'development' ? `http://localhost:3000` : `https://frozen-brushlands-83726.herokuapp.com`
 
 function timeout(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -8,30 +8,30 @@ function timeout(ms) {
 
 export const apiAllTodo = async function() {
     await timeout(1000)
-    const [data] = await Promise.all([customFetch('get', `${baseUrl}/allTodo/`, null)])
+    const [data] = await Promise.all([customFetch('get', `${baseUrl}/api/allTodo/`, null)])
     return data
 }
 
 export const apiCreateTodo = async function(info) {
     await timeout(1000)
-    const [data] = await Promise.all([customFetch('post', `${baseUrl}/create/`, info)])
+    const [data] = await Promise.all([customFetch('post', `${baseUrl}/api/create/`, info)])
     return data
 }
 
 export const apiEditTodo = async function(info) {
     await timeout(1000)
-    const [data] = await Promise.all([customFetch('patch', `${baseUrl}/${info.id}`, info)])
+    const [data] = await Promise.all([customFetch('patch', `${baseUrl}/api/${info.id}`, info)])
     return data
 }
 
 export const apiDeleteTodo = async function(id) {
     await timeout(1000)
-    const [data] = await Promise.all([customFetch('delete', `${baseUrl}/${id}`, null)])
+    const [data] = await Promise.all([customFetch('delete', `${baseUrl}/api/${id}`, null)])
     return data
 }
 
 export const stApiGetTodoById = async function(id) {
     await timeout(1000)
-    const [data] = await Promise.all([customFetch('get', `${baseUrl}/${id}`, null)])
+    const [data] = await Promise.all([customFetch('get', `${baseUrl}/api/${id}`, null)])
     return data
 }
